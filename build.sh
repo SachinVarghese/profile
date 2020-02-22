0111290663f5d76d557bfb46064b0f7bc8d64e17
SachinVarghese <smv@seldon.io>
2020-02-22 23:06:47 +0000

    Adding script for building main page

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-yarn build
-cd build
+cd main
 git init
 git remote add origin https://github.com/SachinVarghese/SachinVarghese.github.io.git
 git add --all
-git commit -m "Updating portfolio"
+git commit -m "Updating github main app"
 git push origin master -f
